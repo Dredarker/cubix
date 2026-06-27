521934a064ecfca739bbea0d167925b286efad18
Ilya <141645849+Dredarker@users.noreply.github.com>
2026-06-27 23:01:05 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -44,8 +44,8 @@ objects.set("map-2.-2", new Obj(-400, -400, 200, 200, "static", "block", "", tru
 objects.set("map-1.-2", new Obj(-200, -400, 200, 200, "static", "block", "", true));
 objects.set("map0.-2", new Obj(0, -400, 200, 200, "static", "block", "", true));
 objects.set("map1.-4", new Obj(200, -800, 200, 200, "static", "block", "", true));
-objects.set("map1.-4ladder", new Obj(200, -800, 50, 800, "static", "ladder", "", true));
-objects.set("map2.-2", new Obj(400, -400, 200, 200, "static", "block", "", true));
+objects.set("map1.-3ladder", new Obj(200, -600, 50, 600, "static", "ladder", "", true));
+objects.set("map1.-3", new Obj(250, -400, 350, 200, "static", "block", "", true));
 objects.set("text", new Text("Here a spawn", "black", new Obj(12, -90, 0, 0, "none", "text", "", true)));
 
 for (let i = 1; i <= 0; i++) {createNPC("Bot "+i, 0, -100)};

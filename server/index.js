563b07f982e3a5d6c17e730e650f67129be5dcd6
Ilya <141645849+Dredarker@users.noreply.github.com>
2026-06-28 00:39:53 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -183,9 +183,7 @@ function posInObj(x, y, obj) {
 }
 
 function distanceBetween(x1, y1, x2, y2) {
-	const dx = x1 - x2;
-	const dy = y1 - y2;
-	return dx*dx+dy*dy;
+	return Math.hypot(x2 - x1, y2 - y1);
 }
 
 function checkUnderCollision(obj) {

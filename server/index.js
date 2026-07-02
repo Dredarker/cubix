ffedc34d67c1cee90df2bb18168808bfcb349dde
Ilya <141645849+Dredarker@users.noreply.github.com>
2026-07-02 18:02:09 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const WebSocket = require("ws");
 const { v4: uuidv4 } = require("uuid");
-import * from './config.js'
+import * from './config.js';
 
 const PORT = process.env.PORT || 3000;
 

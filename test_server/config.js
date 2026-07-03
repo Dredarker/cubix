49e2d2383ea49bcfac89a6adfcc0a007747e12a2
Ilya <141645849+Dredarker@users.noreply.github.com>
2026-07-03 15:02:50 +0700

    Update config.js

--- test_server/config.js
+++ test_server/config.js
@@ -1,17 +1,27 @@
 export let config = {
-	name: "Dreder's Cubix Server",                 // name of server (string)
-	description: "The official Cubix server",      // description of server (string)
-	allowPlayerOutfit: false,                      // can players have custom outfit? (boolean)
-	maxPlayersCount: 10,                           // how many players can play on this server (int)
+	// name of server (string)
+	name: "Test Cubix",
+	// description of server (string)
+	description: "test zone",
+	// can players have custom outfit? (boolean)
+	allowPlayerOutfit: false,
+	// how many players can play on this server (int)
+	maxPlayersCount: 4,
 	game: {
-		gravity: 0.4,                              // how fast falles player and objects (int)
-		useNewCollisionModel: true,                // use other collision model? (boolean)
+		// how fast falles player and objects (int)
+		gravity: 0.3,
+		// use other collision model? (boolean)
+		useNewCollisionModel: false,
 	},
 	player: {
-		colorDefault: "gray",                      // players' outfit, if allowPlayerOutfit is false (string/object)
-		startX: 0,                                 // where players spawns (int)
-		startY: -100,                              // ^^^^^^^^^^^^^^^^^
-		inventory: ["pickaxe", "block", "pistol"], // what a items will get player after joining (array[strings])
-		invsize: 5,                                // how many items can have player (int)
+		// players' outfit, if allowPlayerOutfit is false (string/object)
+		colorDefault: "gray",
+		// where players spawns (int)
+		startX: 0,
+		startY: -1000,
+		// what a items will get player after joining (array[strings])
+		inventory: ["pickaxe", "block", "block", "pistol"],
+		// how many items can have player (int)
+		invsize: 7,
 	},
 }

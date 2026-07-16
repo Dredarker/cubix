dbb31422e8aae2483f8d4128816297d2bbafd17a
Ilya <141645849+Dredarker@users.noreply.github.com>
2026-07-16 18:48:24 +0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,17 +1,31 @@
 export let config = {
-	name: "Dreder's Cubix Server",                 // name of server (string)
-	description: "The official Cubix server",      // description of server (string)
-	allowPlayerOutfit: false,                      // can players have custom outfit? (boolean)
-	maxPlayersCount: 10,                           // how many players can play on this server (int)
+	// name of server (string)
+	name: "Dreder's Cubix Server",
+	// description of server (string)
+	description: "The official Cubix server",
+	// can players have custom outfit? (boolean)
+	allowPlayerOutfit: true,
+	// how many players can play on this server (int)
+	maxPlayersCount: 10,
 	game: {
-		gravity: 0.4,                              // how fast falles player and objects (int)
-		useNewCollisionModel: true,                // use other collision model? (boolean)
+		// how fast falles player and objects (int)
+		gravity: 0.4,
+		// use other collision model? (boolean)
+		useNewCollisionModel: true,
 	},
 	player: {
-		colorDefault: "gray",                      // players' outfit, if allowPlayerOutfit is false (string/object)
-		startX: 0,                                 // where players spawns (int)
-		startY: -100,                              // ^^^^^^^^^^^^^^^^^
-		inventory: ["pickaxe", "block", "pistol"], // what a items will get player after joining (array[strings])
-		invsize: 5,                                // how many items can have player (int)
+		// players' outfit, if allowPlayerOutfit is false (string/object)
+		colorDefault: "gray",
+		// where players spawns (int)
+		startX: 0,
+		startY: -100,
+		// what a items will get player after joining (array[strings])
+		inventory: ["pickaxe", "block", "pistol"],
+		// how many items can have player (int)
+		invsize: 5,
 	},
+	// Webhooks' URLs
+	webhook: {
+		logs: process.env.CubixWebhook
+	}
 }
